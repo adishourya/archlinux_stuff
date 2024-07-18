6daf0d03a313c7be4316f0b135c170d20b671d86
adi <shourya.aditya10@gmail.com>
2024-07-18 20:47:12 +0200

    before something stupid

---
+++ other_installations.sh
@@ -0,0 +1,21 @@
+#make sure github-cli is installed for this
+gh repo clone adishourya/neovim_config ~/.config/nvim
+gh repo clone adishourya/kitty ~/.config/kitty
+
+# oh my zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# fzf-git
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+
+# Anaconda
+# get links from here
+# https://www.anaconda.com/download/success
+
+
+# hyprland
+# git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
+# cd ~/Arch-Hyprland
+# chmod +x install.sh
+# ./install.sh
