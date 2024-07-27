e2dcdee537e5b6e314fd18da81fcc64081b5dad5
adi <shourya.aditya10@gmail.com>
2024-07-27 17:18:08 +0000

    some stuff

--- backup_configurations.sh
+++ backup_configurations.sh
@@ -1,6 +1,6 @@
 cp -r ~/.config/i3 ./configurations/
 cp -r ~/.config/polybar ./configurations/
-cp -r ~/.config/kitty ./configurations/
+cp -r ~/.config/kitty/kitty.conf ./configurations/
 cp -r ~/.config/hypr ./configurations/
 cp -r ~/.config/ags ./configurations/
 cp ~/.zshrc ./configurations/
